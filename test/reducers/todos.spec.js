d9b37442a24015ea744d097f1a6b22a278ec5a30
Matija Marohnić <matija.marohnic@gmail.com>
2016-06-12 22:52:28 +0200

    Use action creator in reducer tests

--- test/reducers/todos.spec.js
+++ test/reducers/todos.spec.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import reducer from 'reducers/todos';
-import { TOGGLE_TODO } from 'actions';
+import { toggleTodo } from 'actions';
 
 test('toggles the todo', t => {
   const prevState = [
@@ -8,8 +8,7 @@ test('toggles the todo', t => {
     { id: 2, text: 'bar', completed: false },
     { id: 3, text: 'baz', completed: false },
   ];
-  const action = { type: TOGGLE_TODO, payload: 2 };
-  const nextState = reducer(prevState, action);
+  const nextState = reducer(prevState, toggleTodo(2));
   t.deepEqual(nextState, [
     { id: 1, text: 'foo', completed: false },
     { id: 2, text: 'bar', completed: true },
